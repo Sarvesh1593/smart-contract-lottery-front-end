a816144c2a6cd2b9e4ce03926e375833cd59ee75
Sarvesh1593 <sarvesh.in.2003@gmail.com>
2025-06-10 12:12:23 +0530

    issue

--- types/web3uikit.d.ts
+++ types/web3uikit.d.ts
@@ -1,4 +1,5 @@
 declare module '@web3uikit/web3' {
-  import Web3 from 'web3'; // Replace with actual types if available.
-  export default Web3;
+  import { FC } from "react";
+
+  export const ConnectButton: FC<any>;
 }
